8676dbd51da57d296bd7376f2c8803ee6a7f9cbe
David Gibbs <david.gibbs@dressipi.com>
2016-10-05 22:01:34 +0100

    Add to and improve meme editor view controller functionality

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // Variables
+    // MARK: Variables
     
-    var keyboardIsShowing = false
     var savedMemes = [Meme]()
+    var currentImagePicked = CGRect()
 
-    // Constants
+    // MARK: Constants
     
     let topText = "TOP TEXT"
     let bottomText = "BOTTOM TEXT"
     let addText = "ADD TEXT"
+    let topConstantDefault: CGFloat = 56
+    let bottomConstantDefault: CGFloat = -56
     
     let memeTextAttributes = [
         NSStrokeColorAttributeName: UIColor.black,
@@ -28,7 +30,7 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         NSStrokeWidthAttributeName: -4.0
     ] as [String : Any]
     
-    // Structs
+    // MARK: Structs
     
     struct Meme {
         let topString: String?
@@ -37,35 +39,47 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         let memeImage: UIImage?
     }
     
-    // IBOutlets
+    // MARK: IBOutlets
     
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var takePhotoButton: UIBarButtonItem!
-    @IBOutlet weak var movableView: UIView!
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var toolBar: UIToolbar!
-    
-    // IBActions
+    @IBOutlet weak var topTextfieldConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var bottomTextFieldConstraintBottom: NSLayoutConstraint!
+
+    // MARK: IBActions
     
     @IBAction func shareButtonPressed(_ sender: UIBarButtonItem) {
+        
+        // Create text to accompany the generated meme
         let shareText = "Check out this cool meme I made 😎"
         let memedImage = generateMemedImage()
+        
         let activityViewController = UIActivityViewController.init(activityItems: [shareText, memedImage], applicationActivities: nil)
-        present(activityViewController, animated: true) {
-            self.savedMemes.append(self.saveMeme())
+        
+        // Save the meme on completion of share
+        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+            if(success){
+                self.savedMemes.append(self.saveMeme())
+            }
         }
+        present(activityViewController, animated: true, completion: nil)
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        
+        // Set UI elements back to default
         topTextField.text = topText
-        topTextField.resignFirstResponder()
         bottomTextField.text = bottomText
+        topTextField.resignFirstResponder()
         bottomTextField.resignFirstResponder()
         imageView.image = nil
         shareButton.isEnabled = false
+        updateTextFieldConstraints(topConstantDefault, bottomConstant: bottomConstantDefault)
     }
     
     @IBAction func takePhotoButtonPressed(_ sender: UIBarButtonItem) {
@@ -80,31 +94,16 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         let pickerController =  UIImagePickerController()
         pickerController.sourceType = sourceType
         pickerController.delegate = self
-        self.present(pickerController, animated: true, completion: nil)
+        present(pickerController, animated: true, completion: nil)
     }
     
-    // ViewController Methods
+    // MARK: ViewController Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Change background colour of status bar
-        
-        let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.size.width, height: 20.0))
-        view.backgroundColor = UIColor.white
-        self.view.addSubview(view)
-        
-        // Set initial appearance of textfields
-        
-        topTextField.delegate = self
-        topTextField.text = topText
-        topTextField.defaultTextAttributes = memeTextAttributes
-        topTextField.textAlignment = .center
-        
-        bottomTextField.delegate = self
-        bottomTextField.text = bottomText
-        bottomTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.textAlignment = .center
+        setupTextFields(topTextField, text: topText)
+        setupTextFields(bottomTextField, text: bottomText)
         
         // Disable the share button initially
         shareButton.isEnabled = false
@@ -121,17 +120,38 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         unsubscribeFromKeyboardNotifications()
     }
     
-    // UITextFieldDelegate methods
+    func setupTextFields(_ textfield: UITextField, text: String) {
+        textfield.delegate = self
+        textfield.text = text
+        textfield.defaultTextAttributes = memeTextAttributes
+        textfield.textAlignment = .center
+    }
+    
+    // MARK: UITextFieldDelegate methods
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        // Clear text if it hasn't been changed from default
         if textField.text == topText || textField.text == bottomText || textField.text == addText {
             textField.text = ""
+        }
+        
+        // Unsubscribe if editing toptextfield, as we don't need the view to move
+        if textField.tag == 0 {
+            unsubscribeFromKeyboardNotifications()
         }
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // Add default text if textfield has been tapped into, but not edited
         if (textField.text?.isEmpty)! {
             textField.text = addText
+        }
+        
+        // Subscribe back to notifications on top textfield when editing ends
+        if textField.tag == 0 {
+            subscribeToKeyboardNotifications()
         }
     }
     
@@ -140,13 +160,20 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         return true
     }
     
-    // UIImagePickerControllerDelegate methods
+    // MARK: UIImagePickerControllerDelegate methods
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         dismiss(animated: true, completion: nil)
+        
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
             shareButton.isEnabled = true
+            
+            currentImagePicked = calculateImageProportions(imageView: imageView)
+        
+            // Update constraints to position textfields in correct positions over image
+            updateTextFieldConstraints((currentImagePicked.origin.y + 10), bottomConstant: -(currentImagePicked.origin.y + 6))
         }
     }
     
@@ -154,29 +181,47 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         dismiss(animated: true, completion: nil)
     }
     
-    // Keyboard notifications
+    func calculateImageProportions(imageView: UIImageView) -> CGRect {
+        let imgViewSize = imageView.frame.size
+        let imgSize = imageView.image?.size
+        
+        let scaleWidth = imgViewSize.width / (imgSize?.width)!
+        let scaleHeight = imgViewSize.height / (imgSize?.height)!
+        let aspect = fmin(scaleWidth, scaleHeight)
+        
+        var imgRect = CGRect(x: 0, y: 0, width: (imgSize?.width)! * aspect, height: (imgSize?.height)! * aspect)
+        
+        imgRect.origin.x = (imgViewSize.width - imgRect.size.width) / 2
+        imgRect.origin.y = (imgViewSize.height - imgRect.size.height) / 2
+        
+        imgRect.origin.x += imageView.frame.origin.x
+        imgRect.origin.y += imageView.frame.origin.y
+        
+        return imgRect
+    }
+    
+    // MARK: Keyboard notifications
     
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(presentKeyboard(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(presentKeyboard(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(dismissKeyboard(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(deviceRotated(_:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
     
+    // MARK: Custom functions
+    
     func presentKeyboard(_ notification: Notification) {
-        switch notification.name {
-        case Notification.Name.UIKeyboardWillShow:
-            if !keyboardIsShowing {
-                bottomTextField.frame.origin.y -= (getKeyboardHeight(notification) - 40)
-                keyboardIsShowing = true
-            }
-        default:
-            bottomTextField.frame.origin.y += (getKeyboardHeight(notification) - 40)
-            keyboardIsShowing = false
-        }
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func dismissKeyboard(_ notification: Notification) {
+        self.view.frame.origin.y += getKeyboardHeight(notification)
     }
     
     func getKeyboardHeight(_ notification: Notification) -> CGFloat {
@@ -185,7 +230,26 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         return keyboardSize.cgRectValue.height
     }
     
-    // Save and generate the meme
+    func deviceRotated(_ notification: Notification) {
+        if (UIDeviceOrientationIsLandscape(UIDevice.current.orientation) || currentImagePicked.isEmpty) {
+            updateTextFieldConstraints(topConstantDefault, bottomConstant: bottomConstantDefault)
+        } else {
+            updateTextFieldConstraints((currentImagePicked.origin.y + 10), bottomConstant: -(currentImagePicked.origin.y + 6))
+        }
+    }
+    
+    func updateTextFieldConstraints(_ topConstant: CGFloat, bottomConstant: CGFloat) {
+        NSLayoutConstraint.deactivate([topTextfieldConstraintTop, bottomTextFieldConstraintBottom])
+        
+        // Add new constraints based on size and position of picked image
+        let newTopAnchor = topTextField.topAnchor.constraint(equalTo: navigationBar.topAnchor, constant: topConstant)
+        let newBottomAnchor = bottomTextField.bottomAnchor.constraint(equalTo: toolBar.bottomAnchor, constant: bottomConstant)
+        
+        topTextfieldConstraintTop = newTopAnchor
+        bottomTextFieldConstraintBottom = newBottomAnchor
+        
+        NSLayoutConstraint.activate([topTextfieldConstraintTop, bottomTextFieldConstraintBottom])
+    }
     
     func saveMeme() -> Meme {
         let savedMeme = Meme(topString: topTextField.text!, bottomString: bottomTextField.text!, originalImage: imageView.image!, memeImage: generateMemedImage())
@@ -204,7 +268,7 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate, UIImagePi
         navigationBar.isHidden = false
         toolBar.isHidden = false
         
-        let crop = CGRect(x: 0, y: (navigationBar.frame.height + CGFloat(20)), width: self.view.frame.width, height: memedImage.size.height - (navigationBar.frame.height + toolBar.frame.height + CGFloat(20)))
+        let crop = CGRect(x: currentImagePicked.origin.x, y: currentImagePicked.origin.y, width: currentImagePicked.width, height: currentImagePicked.height)
         let croppedImage = memedImage.cgImage!.cropping(to: crop)
         
         return UIImage(cgImage: croppedImage!)
